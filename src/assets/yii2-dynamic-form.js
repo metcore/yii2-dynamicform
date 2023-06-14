474bc55fbbab4a9f10a04b7b09bc098ab10dee1a
slamet riyadi <metcore2@gmail.com>
2023-06-14 12:16:27 +0700

    Change js to support select2

--- src/assets/yii2-dynamic-form.js
+++ src/assets/yii2-dynamic-form.js
@@ -3,7 +3,7 @@
  *
  * A jQuery plugin to clone form elements in a nested manner, maintaining accessibility.
  *
- * @author Wanderson Bragança <wanderson.wbc@gmail.com>
+ * @author Wanderson BraganÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬ ÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬ ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â€šÂ¬Ã…Â¡Ãƒâ€šÃ‚Â¬ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â€šÂ¬Ã…Â¾Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬ ÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â€šÂ¬Ã…Â¡Ãƒâ€šÃ‚Â¬ ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬Ãƒâ€¦Ã‚Â¡ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¬ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬Ãƒâ€¦Ã‚Â¾ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬ ÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬ ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬Ãƒâ€¦Ã‚Â¡ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¬ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã‚Â¦ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¡ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬ ÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â€šÂ¬Ã…Â¡Ãƒâ€šÃ‚Â¬ÃƒÆ’Ã¢â‚¬Â¦Ãƒâ€šÃ‚Â¡ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬Ãƒâ€¦Ã‚Â¡ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã…Â¡ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â§a <wanderson.wbc@gmail.com>
  */
 (function ($) {
     var pluginName = 'yiiDynamicForm';
@@ -55,8 +55,8 @@
     };
 
     var _parseTemplate = function(widgetOptions) {
-
         var $template = $(widgetOptions.template);
+
         $template.find('div[data-dynamicform]').each(function(){
             var widgetOptions = eval($(this).attr('data-dynamicform'));
             if ($(widgetOptions.widgetItem).length > 1) {
@@ -66,29 +66,17 @@
         });
 
         $template.find('input, textarea, select').each(function() {
-            if ($(this).is(':checkbox') || $(this).is(':radio')) {
-                var type         = ($(this).is(':checkbox')) ? 'checkbox' : 'radio';
-                var inputName    = $(this).attr('name');
-                var $inputHidden = $template.find('input[type="hidden"][name="' + inputName + '"]').first();
-                var count        = $template.find('input[type="' + type +'"][name="' + inputName + '"]').length;
-
-                if ($inputHidden && count === 1) {
-                    $(this).val(1);
-                    $inputHidden.val(0);
-                }
-
-                $(this).prop('checked', false);
-            } else if($(this).is('select')) {
-                $(this).find('option:selected').removeAttr("selected");
-            } else {
-                $(this).val(''); 
-            }
+            $(this).val('');
         });
 
-        // remove "error/success" css class
-        var yiiActiveFormData = $('#' + widgetOptions.formId).yiiActiveForm('data');
-        $template.find('.' + yiiActiveFormData.settings.errorCssClass).removeClass(yiiActiveFormData.settings.errorCssClass);
-        $template.find('.' + yiiActiveFormData.settings.successCssClass).removeClass(yiiActiveFormData.settings.successCssClass);
+        $template.find('input[type="checkbox"]').each(function() {
+            var inputName = $(this).attr('name');
+            var $inputHidden = $template.find('input[type="hidden"][name="' + inputName + '"]').first();
+            if ($inputHidden) {
+                $(this).val(1);
+                $inputHidden.val(0);
+            }
+        });
 
         return $template;
     };
@@ -107,7 +95,7 @@
         return $elem.closest('.' + widgetOptions.widgetContainer).find(widgetOptions.widgetItem).length;
     };
 
-    var _createIdentifiers = function(level) {
+    var _creatIdentifiers = function(level) {
         return new Array(level + 2).join('0').split('');
     };
 
@@ -115,7 +103,7 @@
         var count = _count($elem, widgetOptions);
 
         if (count < widgetOptions.limit) {
-            $toclone = $(widgetOptions.template);
+            $toclone = widgetOptions.template;
             $newclone = $toclone.clone(false, false);
 
             if (widgetOptions.insertPosition === 'top') {
@@ -139,7 +127,7 @@
             $elem.find('div[data-dynamicform]').each(function() {
                 var currentWidgetOptions = eval($(this).attr('data-dynamicform'));
                 var level           = _getLevel($(this));
-                var identifiers     = _createIdentifiers(level);
+                var identifiers     = _creatIdentifiers(level);
                 var numItems        = $(this).find(currentWidgetOptions.widgetItem).length;
 
                 for (var i = 1; i <= numItems -1; i++) {
@@ -156,7 +144,7 @@
 
             var level          = _getLevel($elem.closest('.' + widgetOptions.widgetContainer));
             var widgetOptionsRoot       = _getWidgetOptionsRoot(widgetOptions);
-            var identifiers    = _createIdentifiers(level);
+            var identifiers    = _creatIdentifiers(level);
             identifiers[0]     = $(widgetOptionsRoot.widgetItem).length - 1;
             identifiers[level] = count - 1;
 
@@ -188,28 +176,20 @@
         }
     };
 
-    var _updateAttrID = function($elem, index) {
-        var widgetOptions = eval($elem.closest('div[data-dynamicform]').attr('data-dynamicform'));
-        var id            = $elem.attr('id');
-        var newID         = id;
+    var _updateAttrID = function(widgetOptions, $elem, index, level) {
+        var id = $elem.attr('id');
+        var newID = id;
 
         if (id !== undefined) {
             var matches = id.match(regexID);
+
             if (matches && matches.length === 4) {
                 matches[2] = matches[2].substring(1, matches[2].length - 1);
                 var identifiers = matches[2].split('-');
                 identifiers[0] = index;
-                
-                if (identifiers.length > 1) {
-                    var widgetsOptions = [];
-                    $elem.parents('div[data-dynamicform]').each(function(i){
-                        widgetsOptions[i] = eval($(this).attr('data-dynamicform'));
-                    });
 
-                    widgetsOptions = widgetsOptions.reverse();
-                    for (var i = identifiers.length - 1; i >= 1; i--) {
-                        identifiers[i] = $elem.closest(widgetsOptions[i].widgetItem).index();
-                    }
+                if (level > 0 && identifiers[level] !== undefined) {
+                    identifiers[level] = $elem.closest(widgetOptions.widgetItem).index();
                 }
 
                 newID = matches[1] + '-' + identifiers.join('-') + '-' + matches[3];
@@ -231,7 +211,7 @@
         return newID;
     };
 
-    var _updateAttrName = function($elem, index) {
+    var _updateAttrName = function(widgetOptions, $elem, index, level) {
         var name = $elem.attr('name');
 
         if (name !== undefined) {
@@ -242,16 +222,8 @@
                 var identifiers = matches[2].split('-');
                 identifiers[0] = index;
 
-                if (identifiers.length > 1) {
-                    var widgetsOptions = [];
-                    $elem.parents('div[data-dynamicform]').each(function(i){
-                        widgetsOptions[i] = eval($(this).attr('data-dynamicform'));
-                    });
-
-                    widgetsOptions = widgetsOptions.reverse();
-                    for (var i = identifiers.length - 1; i >= 1; i--) {
-                        identifiers[i] = $elem.closest(widgetsOptions[i].widgetItem).index();
-                    }
+                if (level > 0 && identifiers[level] !== undefined) {
+                    identifiers[level] = $elem.closest(widgetOptions.widgetItem).index();
                 }
 
                 name = matches[1] + '[' + identifiers.join('][') + ']' + matches[3];
@@ -267,25 +239,34 @@
 
         $(widgetOptionsRoot.widgetItem).each(function(index) {
             var $item = $(this);
+            var level = 0;
+            var currentWidgetOptions = widgetOptionsRoot;
+
             $(this).find('*').each(function() {
+                if ($(this).attr('data-dynamicform') !== undefined) {
+                    currentWidgetOptions = eval($(this).attr('data-dynamicform'));
+                    level = _getLevel($(this));
+                }
+
                 // update "id" attribute
-                _updateAttrID($(this), index);
+                _updateAttrID(currentWidgetOptions, $(this), index, level);
 
                 // update "name" attribute
-                _updateAttrName($(this), index);
+                _updateAttrName(currentWidgetOptions, $(this), index, level);
             });
         });
     };
 
-    var _fixFormValidatonInput = function(widgetOptions, attribute, id, name) {
+    var _fixFormValidatonInput = function(widgetOptions, input, index, id, name) {
+        var attribute = input.baseConfig;
         if (attribute !== undefined) {
-            attribute           = $.extend(true, {}, attribute);
-            attribute.id        = id;
+            attribute = $.extend(true, {}, attribute);
+            attribute.id = id;
             attribute.container = ".field-" + id;
-            attribute.input     = "#" + id;
-            attribute.name      = name;
-            attribute.value     = $("#" + id).val();
-            attribute.status    = 0;
+            attribute.input = "#" + id;
+            attribute.name = input.name.replace("{}", index);
+            attribute.value = $("#" + id).val();
+            attribute.status = 0;
 
             if ($("#" + widgetOptions.formId).yiiActiveForm("find", id) !== "undefined") {
                 $("#" + widgetOptions.formId).yiiActiveForm("remove", id);
@@ -295,57 +276,47 @@
         }
     };
 
-    var _fixFormValidaton = function(widgetOptions) {
-        var widgetOptionsRoot = _getWidgetOptionsRoot(widgetOptions);
+    var _fixFormValidatonInputs = function(widgetOptions, level, index, i) {
+         widgetOptions.fields.forEach(function(input) {
+            var identifiers    = _creatIdentifiers(level);
+            var yiiActiveFormAttribute = $("#" + widgetOptions.formId).yiiActiveForm("find", input.id.replace("{}", identifiers.join('-')));
+            if (yiiActiveFormAttribute !== undefined) {
+                input.baseConfig   = yiiActiveFormAttribute;
+                identifiers[0]     = index;
+                identifiers[level] = i;
+                var id             = input.id.replace("{}", identifiers.join('-'));
+                var name           = input.name.replace("{}", identifiers.join(']['));
 
-        $(widgetOptionsRoot.widgetBody).find('input, textarea, select').each(function() {
-            var id   = $(this).attr('id');
-            var name = $(this).attr('name');
-
-            if (id !== undefined && name !== undefined) {
-                currentWidgetOptions = eval($(this).closest('div[data-dynamicform]').attr('data-dynamicform'));
-                var matches = id.match(regexID);
-
-                if (matches && matches.length === 4) {
-                    matches[2]      = matches[2].substring(1, matches[2].length - 1);
-                    var level       = _getLevel($(this));
-                    var identifiers = _createIdentifiers(level -1);
-                    var baseID      = matches[1] + '-' + identifiers.join('-') + '-' + matches[3];
-                    var attribute   = $("#" + currentWidgetOptions.formId).yiiActiveForm("find", baseID);
-                    _fixFormValidatonInput(currentWidgetOptions, attribute, id, name);
-                }
+                _fixFormValidatonInput(widgetOptions, input, i, id, name);
             }
         });
     };
 
-    var _restoreKrajeeDepdrop = function($elem) {
-        var configDepdrop = $.extend(true, {}, eval($elem.attr('data-krajee-depdrop')));
-        var inputID = $elem.attr('id');
-        var matchID = inputID.match(regexID);
+    var _fixFormValidaton = function(widgetOptions) {
+        var widgetOptionsRoot = _getWidgetOptionsRoot(widgetOptions);
 
-        if (matchID && matchID.length === 4) {
-            for (index = 0; index < configDepdrop.depends.length; ++index) {
-                var match = configDepdrop.depends[index].match(regexID);
-                if (match && match.length === 4) {
-                    configDepdrop.depends[index] = match[1] + matchID[2] + match[3];
+        $(widgetOptionsRoot.widgetItem).each(function(index) {
+            var $item = $(this);
+            var level = 0;
+            var currentWidgetOptions = widgetOptionsRoot;
+            _fixFormValidatonInputs(widgetOptionsRoot, level, index, index);
+
+            $(this).find('div[data-dynamicform]').each(function() {
+                currentWidgetOptions = eval($(this).attr('data-dynamicform'));
+                level = _getLevel($(this));
+
+                if (level > 0) {
+                    $(this).find(currentWidgetOptions.widgetItem).each(function(i) {
+
+                         _fixFormValidatonInputs(currentWidgetOptions, level, index, i);
+                    });
                 }
-            }
-        }
-
-        $elem.depdrop(configDepdrop);
+            });
+        });
     };
 
     var _restoreSpecialJs = function(widgetOptions) {
         var widgetOptionsRoot = _getWidgetOptionsRoot(widgetOptions);
-
-        // "jquery.inputmask"
-        var $hasInputmask = $(widgetOptionsRoot.widgetItem).find('[data-plugin-inputmask]');
-        if ($hasInputmask.length > 0) {
-            $hasInputmask.each(function() {
-                $(this).inputmask('remove');
-                $(this).inputmask(eval($(this).attr('data-plugin-inputmask')));
-            });
-        }
 
         // "kartik-v/yii2-widget-datepicker"
         var $hasDatepicker = $(widgetOptionsRoot.widgetItem).find('[data-krajee-datepicker]');
@@ -428,18 +399,38 @@
         var $hasDepdrop = $(widgetOptionsRoot.widgetItem).find('[data-krajee-depdrop]');
         if ($hasDepdrop.length > 0) {
             $hasDepdrop.each(function() {
-                if ($(this).data('select2') === undefined) {
-                    $(this).removeData().off();
-                    $(this).unbind();
-                    _restoreKrajeeDepdrop($(this));
+                $(this).removeData().off();
+                $(this).unbind();
+                var configDepdrop = eval($(this).attr('data-krajee-depdrop'));
+                var inputID = $(this).attr('id');
+                var matchID = inputID.match(regex);
+                if (matchID && matchID.length === 4) {
+                    for (index = 0; index < configDepdrop.depends.length; ++index) {
+                        var match = configDepdrop.depends[index].match(regex);
+                        if (match && match.length === 4) {
+                            configDepdrop.depends[index] = match[1] + matchID[2] + match[3];
+                        }
+                    }
                 }
+                $(this).depdrop(configDepdrop);
             });
         }
-
         // "kartik-v/yii2-widget-select2"
         var $hasSelect2 = $(widgetOptionsRoot.widgetItem).find('[data-krajee-select2]');
         if ($hasSelect2.length > 0) {
             $hasSelect2.each(function() {
+
+                if ($(this).data('select2') === undefined) {
+
+                    $(this).removeData().off();
+
+                    $(this).unbind();
+                    $(this).val("");
+                    $(this).prop('disabled', false);
+                    $(this).removeAttr("readonly");
+
+                }
+
                 var id = $(this).attr('id');
                 var configSelect2 = eval($(this).attr('data-krajee-select2'));
 
@@ -455,13 +446,13 @@
                     _restoreKrajeeDepdrop($(this));
                 }
 
-                $.when($('#' + id).select2(configSelect2)).done(initSelect2Loading(id, '.select2-container--krajee'));
+                $.when($('#' + id).select2(configSelect2)).done(initS2Loading(id, '.select2-container--krajee'));
 
                 var kvClose = 'kv_close_' + id.replace(/\-/g, '_');
 
-                $('#' + id).on('select2:opening', function(ev) {
-                    initSelect2DropStyle(id, kvClose, ev);
-                });
+                // $('#' + id).on('select2:opening', function(ev) {
+                //     initSelect2DropStyle(id, kvClose, ev);
+                // });
 
                 $('#' + id).on('select2:unselect', function() {
                     window[kvClose] = true;
